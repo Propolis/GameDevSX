b114b8d48637ae44fbca0266ca365a65c9d4fc8e
Petrushaa <logacevz@gmail.com>
2023-11-28 14:14:47 +0300

    меню пробная версия

---
+++ Menu2/Main_Page.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Menu2
+{
+    /// <summary>
+    /// Логика взаимодействия для Main_Page.xaml
+    /// </summary>
+    public partial class Main_Page : Page
+    {
+        public Main_Page()
+        {
+            InitializeComponent();
+            // Создание объекта ImageBrush с ссылкой на изображение
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource = new BitmapImage(new Uri("menu_picture.png", UriKind.Relative)); // Укажите путь к вашему изображению
+
+            // Установка изображения в качестве заднего фона для грида
+            GrMainPage.Background = imageBrush;
+            btSettings.BorderBrush = Brushes.White;
+            btSettings.Background = Brushes.Black;
+            btSettings.Foreground = Brushes.White;
+
+            btPlay.BorderBrush = Brushes.White;
+            btPlay.Background = Brushes.Black;
+            btPlay.Foreground = Brushes.White;
+
+            btGiveUp.BorderBrush = Brushes.White;
+            btGiveUp.Background = Brushes.Black;
+            btGiveUp.Foreground = Brushes.White;
+
+        }
+
+        private void btSettings_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Settings_Page());
+        }
+
+        private void btGiveUp_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Menu2/Settings_Page.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Menu2
+{
+    /// <summary>
+    /// Логика взаимодействия для Settings_Page.xaml
+    /// </summary>
+    public partial class Settings_Page : Page
+    {
+        public Settings_Page()
+        {
+            InitializeComponent();
+            // Создание объекта ImageBrush с ссылкой на изображение
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource = new BitmapImage(new Uri("menu_picture.png", UriKind.Relative)); // Укажите путь к вашему изображению
+
+            // Установка изображения в качестве заднего фона для грида
+            GridSettings.Background = imageBrush;
+
+            btGamePlay.BorderBrush = Brushes.White;
+            btGamePlay.Background = Brushes.Black;
+            btGamePlay.Foreground = Brushes.White;
+
+            btGraphics.BorderBrush = Brushes.White;
+            btGraphics.Background = Brushes.Black;
+            btGraphics.Foreground = Brushes.White;
+
+            btVolume.BorderBrush = Brushes.White;
+            btVolume.Background = Brushes.Black;
+            btVolume.Foreground = Brushes.White;
+
+            btBack.BorderBrush = Brushes.White;
+            btBack.Background = Brushes.Black;
+            btBack.Foreground = Brushes.White;
+        }
+
+        private void btBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
